4d0862a8fcc873787a842ca0dba40fadf14ebcfd
Jaewoong <jaewoong.here@gmail.com>
2015-07-16 00:38:59 +0900

    Testing route

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(cookieParser());
 app.use(require('stylus').middleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
+app.use('/tb/', routes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
