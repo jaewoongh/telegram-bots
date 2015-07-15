0030783c7791ccc5abe5310b55cdd432d5e1a26f
Jaewoong <jaewoong.here@gmail.com>
2015-07-16 00:52:57 +0900

    Testing route

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var routes = require('./routes/bots');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'tb/views'));
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
 // uncomment after placing your favicon in /public
