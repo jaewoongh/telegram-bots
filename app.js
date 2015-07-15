bd341b32ebedd48a343110f2b878d34a179b99a4
Jaewoong <jaewoong.here@gmail.com>
2015-07-16 00:49:31 +0900

    Testing route

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var routes = require('./routes/bots');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'tb/views'));
 app.set('view engine', 'hbs');
 
 // uncomment after placing your favicon in /public
@@ -19,8 +19,8 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(require('stylus').middleware(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(require('stylus').middleware(path.join(__dirname, 'tb/public')));
+app.use(express.static(path.join(__dirname, 'tb/public')));
 
 app.use('/tb/', routes);
 
