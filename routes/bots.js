c84e7e3567bc816c9c2ad4c60f5ef834ca0554aa
Jaewoong <jaewoong.here@gmail.com>
2015-07-16 13:00:03 +0900

    Why not coming Telegram?

--- routes/bots.js
+++ routes/bots.js
@@ -13,14 +13,10 @@ router.get('/test', function(req, res) {
 
 // MrDecisionBot (김결정)
 router.get('/AAFSs16hJE5pqJ4nS78US0WjSUr_d97Uy1M', function(req, res) {
-  if (req.secure) {
-    res.render('index', { title: '김결정 (MrDecisionBot)', description: 'Secure!\nOnly accessible via proper api call.' });
-  } else {
-    res.render('index', { title: '김결정 (MrDecisionBot)', description: 'Only accessible via proper api call.' });
-  }
+  res.render('index', { title: '김결정 (MrDecisionBot)', description: 'Only accessible via proper api call.' });
 });
 router.post('/AAFSs16hJE5pqJ4nS78US0WjSUr_d97Uy1M', function(req, res) {
-  console.log(req.body);
+  console.log('New!', req.body);
   res.send(req.body);
 });
 
