f4ceb12cd8483ad977f40fc09c93da0f0147b1f8
Jaewoong <jaewoong.here@gmail.com>
2015-07-16 12:54:59 +0900

    Trying to figure out how to make HTTPS work

--- app.js
+++ app.js
@@ -22,6 +22,14 @@ app.use(cookieParser());
 app.use(require('stylus').middleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Force redirect to https
+app.use(function(req, res, next) {
+  if (req.headers['x-forwarded-proto'] === 'http') {
+    return res.redirect(['https://', req.get('Host'), req.url].join(''));
+  }
+  next();
+});
+
 app.use('/', bots);
 
 // catch 404 and forward to error handler
