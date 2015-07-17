552679d83332e33f508bc53f83468186994ea4e1
Jaewoong <jaewoong.here@gmail.com>
2015-07-17 23:51:41 +0900

    Added more pattern

--- routes/bots.js
+++ routes/bots.js
@@ -28,17 +28,11 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Telegram bots', description: 'Only accessible via proper api call.' });
 });
 
-router.get('/test', function(req, res) {
-  console.log(req.body);
-  res.send(req.body);
-});
-
 // MrDecisionBot (김결정)
 var mrDecisionBot = require('../lib/MrDecisionBot');
 router.get('/AAFSs16hJE5pqJ4nS78US0WjSUr_d97Uy1M', function(req, res) {
   res.render('index', {
-    title: 'Telegram Bots',
-    botname: '김결정 (MrDecisionBot)',
+    title: '김결정 (@MrDecisionBot)',
     description: '결정은 내가 한다!',
     bottoken: mrDecisionBot.botToken,
     bottoken_short: mrDecisionBot.botToken_short });
